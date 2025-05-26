f9ec0cbd6ae7aa84226b7f569cacc3edff437edd
Pranjali Pradhan <pranjali01p@gmail.com>
2025-05-26 17:54:42 +0530

    edit,delete and markAs

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -2,13 +2,26 @@ import React, { useState } from "react";
 import { v4 as uuidv4 } from "uuid";
 
 export default function TodoList() {
-  let [todos, setTodos] = useState([{ task: "sample", id: uuidv4() }]);
+  let [todos, setTodos] = useState([
+    { task: "sample-task", id: uuidv4(), isDone: false },
+  ]);
   let [newTodo, setNewTodo] = useState("");
+  let [editId, setEditId] = useState(null);
 
   let addNewTask = () => {
-    setTodos((prevTodos) => {
-      return [...prevTodos, { task: newTodo, id: uuidv4() }];
-    });
+    if (editId) {
+      // Update existing task
+      let updatedTodos = todos.map((todo) =>
+        todo.id === editId ? { ...todo, task: newTodo, isDone: false } : todo
+      );
+      setTodos(updatedTodos);
+      setEditId(null);
+    } else {
+      setTodos((prevTodos) => [
+        ...prevTodos,
+        { task: newTodo, id: uuidv4(), isDone: false },
+      ]);
+    }
     setNewTodo("");
   };
 
@@ -17,7 +30,43 @@ export default function TodoList() {
   };
 
   let deleteTodo = (id) => {
-    setTodos((prevTodos) => todos.filter((prevTodos) => todo.id != id));
+    setTodos((prevTodos) => prevTodos.filter((todo) => todo.id !== id));
+    if (editId === id) {
+      setEditId(null);
+      setNewTodo("");
+    }
+  };
+
+  let editTodo = (id) => {
+    let selectedTodo = todos.find((todo) => todo.id === id);
+    setNewTodo(selectedTodo.task);
+    setEditId(id);
+  };
+
+  let markAll = () => {
+    setTodos((prevTodos) =>
+      prevTodos.map((todo) => {
+        return {
+          ...todo,
+          isDone: true,
+        };
+      })
+    );
+  };
+
+  let markAsDone = (id) => {
+    setTodos((prevTodos) =>
+      prevTodos.map((todo) => {
+        if (todo.id == id) {
+          return {
+            ...todo,
+            isDone: true,
+          };
+        } else {
+          return todo;
+        }
+      })
+    );
   };
 
   return (
@@ -29,17 +78,32 @@ export default function TodoList() {
         onChange={updateTodovalue}
         placeholder="Enter task"
       />
-      <button onClick={addNewTask}>Add Task</button>
+      <button onClick={addNewTask}>
+        {editId ? "Update Task" : "Add Task"}
+      </button>
       <br />
 
       <ul>
         {todos.map((todo) => (
-          <li key={todo.id}>
-            <span>{todo.task}</span>
-            <button onClick={() => deleteTodo(todo.id)}>Delete</button>
-          </li>
+          <div className="box">
+            <li key={todo.id}>
+              <span
+                style={
+                  todo.isDone ? { textDecorationLine: "line-through" } : {}
+                }
+              >
+                {todo.task}
+              </span>
+            </li>
+            <div>
+              <button onClick={() => deleteTodo(todo.id)}>Delete</button>
+              <button onClick={() => editTodo(todo.id)}>Edit</button>
+              <button onClick={() => markAsDone(todo.id)}>Done</button>
+            </div>
+          </div>
         ))}
       </ul>
+      <button onClick={markAll}>Mark All as Done</button>
     </div>
   );
 }
